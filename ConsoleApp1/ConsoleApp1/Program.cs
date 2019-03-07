232ea37ba1f8a9984ccefdac3bdf084be5ad1685
visualstudio <visualstudio@230-PC06>
2019-03-07 13:37:32 -0500

    1:37PM

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿//GAGANDEEP SINGH  C0730407
+//RANDEEP KAUR  C0731777
+//CSD3354 Section 1
+//ASSIGNMENT 2
+//March 7,2019
+//LAB1
+using System;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public delegate void MyDelegate();
+
+        public void Method1()
+        {
+            Console.WriteLine("Method1");
+            Console.ReadLine();
+        }
+        public void Method2()
+        {
+            MyDelegate mydelegate = new MyDelegate(Method1);
+            mydelegate();
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
