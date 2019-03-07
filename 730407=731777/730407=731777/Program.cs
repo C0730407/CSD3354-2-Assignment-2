b3704a6134eea982f2009cd38c37e8049494fe78
visualstudio <visualstudio@230-PC06>
2019-03-07 15:53:41 -0500

    lab15

--- 730407=731777/730407=731777/Program.cs
+++ 730407=731777/730407=731777/Program.cs
@@ -10,34 +10,40 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace _730407_731777
+{ 
+    public class Program
     {
-        public class Program
+        public static void Main()
         {
-            public static void Main()
+            DelegateExercises delegateExercises = new DelegateExercises();
+            try
             {
-                DelegateExercises delegateExercises = new DelegateExercises();
                 delegateExercises.Method3();
                 Console.ReadLine();
             }
-        }
-        public delegate int MyDelegate(out int i);
+            catch (System.Exception ex)
+            {
+                System.Console.WriteLine("Exception Occurred.");
+                Console.ReadLine();
 
-        public class DelegateExercises
-        {
-            int Method1(out int i)
-            {
-                i = 100;
-                System.Console.WriteLine("Method1 " + i);
-                return 0;
-            }
-            public void Method3()
-            {
-                MyDelegate myDelegate = new MyDelegate(Method1);
-                MyDelegate myDelegate1 = null;
-                MyDelegate myDelegate2 = myDelegate + myDelegate1;
-                int intValue;
-                myDelegate2(out intValue);
             }
         }
-
     }
+
+    public delegate void MyDelegate();
+
+    public class DelegateExercises
+    {
+        void Method1()
+        {
+            throw new System.Exception();
+        }
+
+        public void Method3()
+        {
+            MyDelegate myDelegate = new MyDelegate(Method1);
+            myDelegate();
+        }
+    }
+
+}
