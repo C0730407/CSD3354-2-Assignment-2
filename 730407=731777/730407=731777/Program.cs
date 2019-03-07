b4d4bacc5b48c17b567afcce881618fd6865dec4
visualstudio <visualstudio@230-PC06>
2019-03-07 15:31:48 -0500

    lab10

--- 730407=731777/730407=731777/Program.cs
+++ 730407=731777/730407=731777/Program.cs
@@ -11,9 +11,34 @@ using System.Threading.Tasks;
 
 namespace _730407_731777
 {
-    public delegate void MyDelegate();
-    public class DelegateExcercises:MyDelegate
+    public class Program
     {
+        public static void Main()
+        {
+            DelegateExercises delegateExercises = new DelegateExercises();
+            delegateExercises.Method3();
+            Console.ReadLine();
+        }
+    }
 
+    public delegate void MyDelegate();
+
+    public class DelegateExercises
+    {
+        void Method1()
+        {
+            System.Console.WriteLine("Method1");
+
+
+        }
+
+        public void Method3()
+        {
+            MyDelegate myDelegate = new MyDelegate(Method1);
+            myDelegate();
+            System.Console.WriteLine(myDelegate.ToString());
+        }
     }
 }
+
+  
