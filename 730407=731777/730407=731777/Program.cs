81f95ad954cda52f7fd4824cd607f5e6cf6203c4
visualstudio <visualstudio@230-PC06>
2019-03-07 15:10:43 -0500

    lab06

--- 730407=731777/730407=731777/Program.cs
+++ 730407=731777/730407=731777/Program.cs
@@ -17,20 +17,32 @@ namespace _730407_731777
         {
             DelegateExcercises delegateExcercises = new DelegateExcercises();
             delegateExcercises.Method3();
+            Console.ReadLine();
         }
     }
     public class DelegateExcercises
     {
-        public delegate int MyDelegate();
+        public delegate int MyDelegate(int intValue);
 
-        void Method1()
+        int  Method1(int intMethod1)
         {
-            System.Console.WriteLine("MyDelegate");
+            return intMethod1 * 2;
         }
-       public void Method3()
+        int Method2(int intMethod1)
+        {
+            return intMethod1 * 10;
+        }
+            public void Method4(MyDelegate myDelegate)
+        {
+            int result = myDelegate(10);
+            Console.WriteLine(result);
+        }
+               public void Method3() 
         {
             MyDelegate myDelegate = new MyDelegate(Method1);
-            myDelegate();
+            Method4(myDelegate);
+            myDelegate = new MyDelegate(Method2);
+            Method4(myDelegate);
         }
     }
 }
