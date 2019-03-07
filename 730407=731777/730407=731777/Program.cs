abf00b95acde2b11ce02e81514acdc587f048979
visualstudio <visualstudio@230-PC06>
2019-03-07 15:48:52 -0500

    lab13

--- 730407=731777/730407=731777/Program.cs
+++ 730407=731777/730407=731777/Program.cs
@@ -11,39 +11,36 @@ using System.Threading.Tasks;
 
 namespace _730407_731777
 {
-    using System;
-
-    delegate void ExampleDelegate(string xyz);
-
-    class Program
-    {
-        public static void Method1(string xyz)
+        public class Program
         {
-            Console.WriteLine(xyz + " Method1");
+            public static void Main()
+            {
+                DelegateExercises delegateExercises = new DelegateExercises();
+                delegateExercises.Method3();
+                Console.ReadLine();
+            }
         }
 
-        public static void Method2(string xyz)
+        public delegate int MyDelegate(out int i);
+
+        public class DelegateExercises
         {
-            Console.WriteLine(xyz + " Method2");
+            int Method1(out int i)
+            {
+                System.Console.WriteLine("Method1");
+                i = 10;
+                return 0;
+            }
+
+            public void Method3()
+            {
+                MyDelegate myDelegate = new MyDelegate(Method1);
+                MyDelegate myDelegate1 = new MyDelegate(Method1);
+                MyDelegate myDelegate2 = myDelegate + myDelegate1;
+                int intValue;
+                myDelegate2(out intValue);
+
+            }
         }
 
-        public static void Main()
-        {
-            ExampleDelegate ex1Delegate, ex2Delegate, ex3Delegate, myDelegate;
-
-            ex1Delegate = new ExampleDelegate(Method1);
-            ex2Delegate = new ExampleDelegate(Method2);
-            ex3Delegate = ex1Delegate + ex2Delegate;
-            myDelegate = ex1Delegate - ex2Delegate;
-            ex1Delegate("AAA");
-            ex2Delegate("BBB");
-            ex3Delegate("CCC");
-            myDelegate("DDD");
-            myDelegate = ex3Delegate - ex1Delegate;
-            myDelegate("EEE");
-            myDelegate = ex3Delegate - ex2Delegate;
-            myDelegate("FFF");
-            Console.ReadLine();
-        }
     }
-}
